5d7b9becad655c5317d9a2a637607eff46042a19
Mainaa <mnbr206@gmail.com>
2022-01-18 20:39:54 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -7,12 +7,17 @@ var scores = [0, 0];
 //тоглогчийн ээлжиндээ цуглуулах оноог хадгалах хувьсагч
 var roundScore = 0;
 //Шооны аль талаар буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() *6) + 1;
+// var dice = Math.floor(Math.random() *6) + 1;
 {/* <div class="player-score" id="score-0">43</div> */}
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
-document.querySelector(".dice").style.display = "none";
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+var diceDom = document.querySelector(".dice")
+// diceDom.style.display = "none";
 
-console.log("шоо :" + dice);
+document.querySelector(".btn-roll").addEventListener("click",function() {
+    var diceNumber = Math.floor(Math.random() *6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + diceNumber + ".png";
+})
