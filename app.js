48c8ec40e3644627f9c6bb3f0c134c87840edfa4
Mainaa <mnbr206@gmail.com>
2022-01-19 10:38:19 +0800

    onoo tsugluuldag bolson

--- app.js
+++ app.js
@@ -14,10 +14,27 @@ document.getElementById("score-1").textContent = 0;
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 var diceDom = document.querySelector(".dice")
-// diceDom.style.display = "none";
-
+diceDom.style.display = "none";
+// санамсаргүй тоо гаргаж авна
 document.querySelector(".btn-roll").addEventListener("click",function() {
     var diceNumber = Math.floor(Math.random() *6) + 1;
+
     diceDom.style.display = "block";
+    // тоогоо шоон дээр харуулах
     diceDom.src = "dice-" + diceNumber + ".png";
-})
+    // буусан тоо 1 ээс ялгаатай бол оноо нэмэгдэнэ.
+    if (diceNumber !== 1) {
+        roundScore = roundScore + diceNumber;
+        document.getElementById("current-" + activePlayer).textContent = roundScore
+    }   else {
+        roundScore = 0;
+        document.getElementById("current-" + activePlayer).textContent = 0;
+
+        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+        document.querySelector(".player-0-panel").classList.remove("active");
+        document.querySelector(".player-1-panel").classList.add("active")
+        diceDom.style.display = "none"; 
+
+    }
+    
+});
